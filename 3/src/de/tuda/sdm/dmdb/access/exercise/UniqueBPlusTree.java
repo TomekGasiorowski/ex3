73497633895376f84e70f01b59a952dbcc810d40
Nam Nguyen <Nam Nguyen@NamNguyen-Notebook>
2017-11-14 11:37:14 +0100

    lookup UBtree

--- 3/src/de/tuda/sdm/dmdb/access/exercise/UniqueBPlusTree.java
+++ 3/src/de/tuda/sdm/dmdb/access/exercise/UniqueBPlusTree.java
@@ -9,50 +9,50 @@ import de.tuda.sdm.dmdb.storage.types.AbstractSQLValue;
 import de.tuda.sdm.dmdb.storage.types.SQLInteger;
 
 /**
- * Unique B+-Tree implementation 
+ * Unique B+-Tree implementation
+ * 
  * @author cbinnig
  *
  * @param <T>
  */
 public class UniqueBPlusTree<T extends AbstractSQLValue> extends UniqueBPlusTreeBase<T> {
-	
+
 	/**
 	 * Constructor of B+-Tree with user-defined fil-grade
-	 * @param table Table to be indexed
-	 * @param keyColumnNumber Number of unique column which should be indexed
-	 * @param fillGrade fill grade of index
+	 * 
+	 * @param table
+	 *            Table to be indexed
+	 * @param keyColumnNumber
+	 *            Number of unique column which should be indexed
+	 * @param fillGrade
+	 *            fill grade of index
 	 */
 	public UniqueBPlusTree(AbstractTable table, int keyColumnNumber, int fillGrade) {
 		super(table, keyColumnNumber, fillGrade);
-	} 
-	
+	}
+
 	/**
 	 * Constructor for B+-tree with default fill grade
-	 * @param table table to be indexed 
-	 * @param keyNumber Number of unique column which should be indexed
+	 * 
+	 * @param table
+	 *            table to be indexed
+	 * @param keyNumber
+	 *            Number of unique column which should be indexed
 	 */
 	public UniqueBPlusTree(AbstractTable table, int keyColumnNumber) {
 		this(table, keyColumnNumber, DEFAULT_FILL_GRADE);
-	}	
-	
+	}
+
 	@SuppressWarnings({ "unchecked" })
 	@Override
 	public boolean insert(AbstractRecord record) {
-		//insert record
+		// insert record
 		T key = (T) record.getValue(this.keyColumnNumber);
 		return this.getRoot().insert(key, record);
 	}
-	
+
 	@Override
 	public AbstractRecord lookup(T key) {
-		if (this.getRoot() instanceof Leaf){
-			System.out.println("aa");
-			return this.getRoot().lookup(key);
-		}
-		else {
-			System.out.println("bb");
-			int sep = this.getRoot().binarySearch(key);
-			return this.indexElements.get(sep).lookup(key);
-		}
+		return this.getRoot().lookup(key);
 	}
 }
