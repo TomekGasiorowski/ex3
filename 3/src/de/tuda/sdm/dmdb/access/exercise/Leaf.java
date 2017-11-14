17e971feb1c59c8ac3ecf3b3a233148ad73bb9c1
Gasiorowski <tomekgasiorowski@hotmail.com>
2017-11-14 16:54:08 +0100

    leaf

--- 3/src/de/tuda/sdm/dmdb/access/exercise/Leaf.java
+++ 3/src/de/tuda/sdm/dmdb/access/exercise/Leaf.java
@@ -10,6 +10,7 @@ import de.tuda.sdm.dmdb.access.UniqueBPlusTreeBase;
 import de.tuda.sdm.dmdb.storage.AbstractPage;
 import de.tuda.sdm.dmdb.storage.AbstractRecord;
 import de.tuda.sdm.dmdb.storage.Record;
+import de.tuda.sdm.dmdb.storage.RowPage;
 import de.tuda.sdm.dmdb.storage.types.AbstractSQLValue;
 import de.tuda.sdm.dmdb.storage.types.SQLInteger;
 import de.tuda.sdm.dmdb.storage.types.SQLVarchar;
@@ -42,12 +43,13 @@ public class Leaf<T extends AbstractSQLValue> extends LeafBase<T> {
 
 	@Override
 	public boolean insert(T key, AbstractRecord record) {
+		System.out.println(this.getUniqueBPlusTree().getTable().lookup(2, 0));
 		AbstractRecord rec = this.getUniqueBPlusTree().getLeafRecPrototype().clone();
 		this.binarySearch(key, rec);
-		if (rec.getValue(UniqueBPlusTreeBase.KEY_POS).compareTo(key) == 0) {
+		//if (rec.getValue(UniqueBPlusTreeBase.KEY_POS).compareTo(key) == 0) {
 			// replace old value
-		} else {
-
+		//} 
+		//else {
 			RowIdentifier rowID = this.getUniqueBPlusTree().getTable().insert(record);
 			AbstractRecord localRec = this.getUniqueBPlusTree().getLeafRecPrototype().clone();
 
@@ -55,14 +57,19 @@ public class Leaf<T extends AbstractSQLValue> extends LeafBase<T> {
 			localRec.setValue(this.getUniqueBPlusTree().PAGE_POS, new SQLInteger(rowID.getPageNumber()));
 			localRec.setValue(this.getUniqueBPlusTree().SLOT_POS, new SQLInteger(rowID.getSlotNumber()));
 
-			this.getIndexPage().insert(localRec);
-
+			int i = this.getIndexPage().insert(localRec);
 			if (this.isFull()) {
 				AbstractIndexElement<T> leaf1 = this.createInstance();
 				AbstractIndexElement<T> leaf2 = this.createInstance();
 				this.split(leaf1, leaf2);
+				AbstractIndexElement<T> newnode = new Node<T>(this.getUniqueBPlusTree());
+				AbstractRecord r = this.getUniqueBPlusTree().getLeafRecPrototype().clone();
+				leaf2.getIndexPage().read(0, r);
+				newnode.getIndexPage().insert(r);
+				AbstractPage ap = new RowPage(this.getUniqueBPlusTree().getNodeRecPrototype().getFixedLength());
+				
 			}
-		}
+		//}
 		return true;
 	}
 
